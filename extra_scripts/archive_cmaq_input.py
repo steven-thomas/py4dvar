2974a38aea720178508f1b4e4222ab883ae34a30
Steven Thomas <stevenpt@unimelb.edu.au>
2017-11-29 11:36:06 +1100

    added extra_scripts dir for utility/analysis scripts & archive_cmaq script (for recording cmaq input responsible for prior)

---
+++ extra_scripts/archive_cmaq_input.py
@@ -0,0 +1,16 @@
+"""
+Copies current cmaq input files into archive.
+"""
+import _get_root
+import fourdvar.util.archive_handle as archive_handle
+import fourdvar.params.archive_defn as archive_defn
+import fourdvar.datadef as d
+
+# archive file name
+archive_fname = 'original_CMAQ_input'
+
+archive_handle.archive_path = archive_defn.archive_path
+archive_handle.finished_setup = True
+
+model_input = d.ModelInputData()
+model_input.archive( archive_fname )
