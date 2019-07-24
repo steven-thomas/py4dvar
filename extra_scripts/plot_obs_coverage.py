30f69bf4673a23e1fc3e43d884889138d90e9bba
Steven Thomas <stevenpt@unimelb.edu.au>
2019-07-24 14:05:43 +1000

    added a timeseries to the plot_obs_coverage extra script.

--- extra_scripts/plot_obs_coverage.py
+++ extra_scripts/plot_obs_coverage.py
@@ -1,20 +1,26 @@
 
 import os
 import numpy as np
+import datetime
 import matplotlib.pyplot as plt
 
 import context
-import fourdvar.datadef as d
 import fourdvar.user_driver as user
-import fourdvar.params.input_defn as input_defn
-from fourdvar.params.root_path_defn import store_path
+import fourdvar.util.date_handle as dt
 
 obs = user.get_observed()
 
 nrow = obs.grid_attr['NROWS']
 ncol = obs.grid_attr['NCOLS']
+nday = len( dt.get_datelist() )
+
+tstep = obs.grid_attr['TSTEP']
+tsec = (tstep//10000)*60*60 + ((tstep//100)%100)*60 + tstep%100
+daysec = 24*60*60
+nstep = daysec/tsec
 
 coverage = np.zeros((nrow,ncol,),dtype=int)
+obs_step = []
 
 skipped_obs = 0
 for weight in obs.weight_grid:
@@ -24,17 +30,30 @@ for weight in obs.weight_grid:
         continue
     #assign obs to surface cell with largest weight
     _,obs_coord = max([ (v,k,) for k,v in weight.items() if k[2]==0 ])
-    #only need the row & col of the obs
-    r,c = (obs_coord[3],obs_coord[4],)
+    d,t,_,r,c,_ = obs_coord
     coverage[r,c] += 1
+    date = datetime.datetime.strptime( str( d ), '%Y%m%d' ).date()
+    obs_step.append( (date-dt.start_date).days*nstep + t )
 if skipped_obs > 0:
     print '{:} non-surface obs were omitted.'
 
 omax = coverage.max()
 
+fig = plt.figure( figsize=(10,5) )
+plt.subplot(121)
 plt.title( '# of observations for each surface gridcell' )
-cov = plt.pcolormesh( coverage, vmin=0, cmap='Blues' )
+cov = plt.pcolormesh( coverage, vmin=0, cmap='Blues',
+                      edgecolor='k', linestyle=':', linewidth=.1 )
 cbar = plt.colorbar()
 cbar.set_label('# of observations')
 cbar.set_ticks( range(0,omax,max(1,omax//5)) )
+
+plt.subplot(122)
+plt.title( '# of observations for each timestep' )
+plt.hist( obs_step, bins=[ i+.5 for i in range(nday*nstep+1) ] )
+plt.xlim(0,nday*nstep+1)
+plt.xticks( [ nstep/2 + nstep*i for i in range(nday) ],
+            [ dt.replace_date('<YYYY-MM-DD>',d) for d in dt.get_datelist() ] )
+for i in range(1,nday):
+    plt.axvline( i*nstep, ls=':', c='k' )
 plt.show()
