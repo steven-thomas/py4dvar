24b435255318d4bdfef1f9c552bcbb1817ec3110
Steven Thomas <stevenpt@spartan-login2.hpc.unimelb.edu.au>
2020-04-20 09:23:53 +1000

    update submit scripts and No. of cmaq processesors.

--- obs_preprocess/submit.sh
+++ obs_preprocess/submit.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --partition=physical
 #SBATCH --ntasks=1
-#SBATCH --time=12:00:00
-#SBATCH --job-name=fourdvar_main
+#SBATCH --time=1:00:00
+#SBATCH --job-name=obs_preprocess
 
 module purge
 module load netCDF
@@ -12,4 +12,4 @@ module load OpenMPI/3.1.0-iccifort-2018.u4-GCC-8.2.0-cuda9-ucx
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/usr/local/easybuild/software/netCDF/4.5.0-spartan_intel-2017.u2/lib64"
 
 
-python runscript.py >& output.txt
+python tropomi_co_preprocess.py >& output_tropomi_preprocess.txt
--- tests/submit.sh
+++ tests/submit.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 #SBATCH --partition=physical
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --nodes=1
-#SBATCH --time=1:00:00
-#SBATCH --job-name=fourdvar_main
+#SBATCH --ntasks=12
+#SBATCH --time=12:00:00
+#SBATCH --job-name=run_p4d_test
 
 module purge
 module load netCDF
@@ -13,7 +11,5 @@ module load netCDF-Fortran
 module load OpenMPI/3.1.0-iccifort-2018.u4-GCC-8.2.0-cuda9-ucx
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/usr/local/easybuild/software/netCDF/4.5.0-spartan_intel-2017.u2/lib64"
 
-
-cd /home/stevenpt/fourdvar/py4dvar/tests
 
 python pert_pert_test.py >& output_pert_pert.txt
