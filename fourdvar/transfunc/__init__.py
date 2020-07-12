f51424f507da3f8c9e11ff1b25bd14e481561fbe
Steven Thomas <stevenpt@spartan-login1.hpc.unimelb.edu.au>
2020-07-12 17:11:26 +1000

    added licence to project.

--- fourdvar/transfunc/__init__.py
+++ fourdvar/transfunc/__init__.py
@@ -1,13 +1,11 @@
 """
-framework: turns directory into a package
-packages can be imported
-eg: from <package> import <module>
+__init__.py
 
-any code in this file is run the first time this package is imported
-
-imports listed are for easier naming and importing
-
-eg: "import transfunc.run_model" instead of "import transfunc.run_model.run_model"
+Copyright 2016 University of Melbourne.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
 """
 
 from fourdvar.transfunc.calc_forcing import calc_forcing
