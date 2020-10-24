3e1d29bbcad04de6ab6e6f4feb13239edc0c819e
Peter Rayner <prayner@unimelb.edu.au>
2020-10-24 12:21:16 +1100

    rearranging imports

--- fourdvar/sector/electricityGenerationSector.py
+++ fourdvar/sector/electricityGenerationSector.py
@@ -6,10 +6,14 @@ Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
 You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and limitations under the License.
+
+
 """
-import config
-import baseSector
+
 import numpy as np
+
+from foudvar.params import config
+import baseSector
 class ElectricityGenerationSector( baseSector.BaseSector):
     def __init__( self, initval):
         baseSector.BaseSector.__init__( self)
