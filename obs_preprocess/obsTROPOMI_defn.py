2757467b217583ee0fb2c568dd2142a6730014bd
Caitlin Minney <cm5310@gadi-login-09.gadi.nci.org.au>
2021-10-24 12:41:19 +1100

    Committer: Caitlin Minney <cm5310@gadi-login-09.gadi.nci.org.au>  On branch cminneyCO  Changes to be committed: 	new file:   obsTROPOMI_defn.py 	new file:   oco2lite_preprocess.py.bak 	new file:   ray_trace.py.bak 	new file:   super_obs_util.py
     Changes not staged for commit:
    	modified:   ../fourdvar/params/root_path_defn.py
    	modified:   model_space.py
    	deleted:    obsOCO2_defn.py
    	modified:   oco2lite_preprocess.py
    	modified:   ray_trace.py
    	modified:   ../yohanna_py4dvar/py4dvar (modified content)

---
+++ obs_preprocess/obsTROPOMI_defn.py
@@ -0,0 +1,151 @@
+"""
+obsOCO2_defn.py
+
+Copyright 2016 University of Melbourne.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
+"""
+
+from __future__ import absolute_import
+
+import datetime as dt
+import numpy as np
+
+from obs_defn import ObsMultiRay
+from ray_trace import Point, Ray
+
+class ObsTROPOMI( ObsMultiRay ):
+    """Single observation (or sounding) from OCO2 satellite
+    This observation class only works for 1 species.
+    """
+    required = ['value','uncertainty','weight_grid','offset_term']
+    
+    @classmethod
+    def create( cls, **kwargs ):
+        """kwargs comes from variables in oco2 file.
+        min. requirements for kwargs:
+        - sounding_id : long_int
+        - latitude : float (degrees)
+        - longitude : float (degrees)
+        - time : float (unix timestamp)
+        - solar_zenith_angle : float (degrees)
+        - sensor_zenith_angle : float (degrees)
+        - solar_azimuth_angle : float (degrees)
+        - sensor_azimuth_angle : float (degrees)
+        - warn_level : byte_int
+        - xco2 : float (ppm)
+        - xco2_uncertainty : float (ppm)
+        - xco2_apriori : float (ppm)
+        - co2_profile_apriori : array[ float ] (length=levels, units=ppm)
+        - xco2_averaging_kernel : array[ float ] (length=levels)
+        - pressure_levels : array[ float ] (length=levels, units=hPa)
+        - pressure_weight : array[ float ] (length=levels)
+        """
+        newobs = cls( obstype='TROPOMI_sounding' )
+        newobs.out_dict['value'] = kwargs['co_column']
+        newobs.out_dict['uncertainty'] = kwargs['co_column_precision']
+        
+        # newobs.out_dict['OCO2_id'] = kwargs['sounding_id']
+        newobs.out_dict['surface_type'] = kwargs['surface_type']
+        newobs.out_dict['operation_mode'] = kwargs['operation_mode']
+        newobs.out_dict['quality_flag'] = kwargs['quality_flag']
+        #OCO2 Lite-files only record CO2 values
+        newobs.spcs = 'CO'
+        newobs.src_data = kwargs.copy()
+        return newobs
+    
+    def model_process( self, model_space ):
+        ObsMultiRay.model_process( self, model_space )
+        #now created self.out_dict[ 'weight_grid' ]
+        return None
+    
+    def add_visibility( self, proportion, model_space ):
+        #obs pressure is in hPa, convert to model units (Pa)
+        obs_pressure = 100. * np.array( self.src_data[ 'pressure_levels' ] )
+        obs_kernel = np.array( self.src_data[ 'co_column_precision' ] )
+        obs_apriori = np.array( self.src_data[ 'co_profile_apriori' ] )
+        
+        #get sample model coordinate at surface
+        coord = [ c for c in proportion.keys() if c[2] == 0 ][0]
+        
+        model_pweight = model_space.get_pressure_weight( coord )
+        model_kernel = model_space.pressure_interp( obs_pressure, obs_kernel, coord )
+        model_apriori = model_space.pressure_interp( obs_pressure, obs_apriori, coord )
+        
+        model_vis = model_pweight * model_kernel
+        column_xco = ( model_pweight * model_kernel * model_apriori )
+        self.out_dict['offset_term'] = self.src_data['co_column_apriori'] - column_xco.sum()
+        
+        weight_grid = {}
+        for l, weight in enumerate( model_vis ):
+            lrtainty : float (ppm)
+            co_column_apriori : float (ppm)
+            co_column_apriori : array[ float ] (length=levels, units=ppm)
+            co_column_averaging_kernel : array[ float ] (length=levels)
+            pressure_levels : array[ float ] (length=levels, units=hPa)
+            pressure_weight : array[ float ] (length=levels)
+        """
+        newobs = cls( obstype='OCO2_sounding' )
+        newobs.out_dict['value'] = kwargs['xco2']
+        newobs.out_dict['uncertainty'] = kwargs['xco2_uncertainty']
+
+        # newobs.out_dict['OCO2_id'] = kwargs['sounding_id']
+        newobs.out_dict['surface_type'] = kwargs['surface_type']
+        newobs.out_dict['operation_mode'] = kwargs['operation_mode']
+        newobs.out_dict['xco2_quality_flag'] = kwargs['xco2_quality_flag']
+        #OCO2 Lite-files only record CO2 values
+        newobs.spcs = 'CO2'
+        newobs.src_data = kwargs.copy()
+        return newobs
+ = { c:v for c,v in proportion.items() if c[2] == l }
+            lyer_sum = sum( layer_slice.values() )
+            weight_slice = { c: weight*v/layer_sum for c,v in layer_slice.items() }
+            weight_grid.update( weight_slice )
+        
+        return weight_grid
+    
+    def map_location( self, model_space ):
+        assert model_space.gridmeta['GDTYP'] == 2, 'invalid GDTYP'
+        #convert source location data into a list of spacial points
+        lat = self.src_data[ 'latitude' ]
+        lon = self.src_data[ 'longitude' ]
+        p0_zenith = np.radians( self.src_data[ 'solar_zenith_angle' ] )
+        p0_azimuth = np.radians( self.src_data[ 'solar_azimuth_angle' ] )
+        p2_zenith = np.radians( self.src_data[ 'sensor_zenith_angle' ] )
+        p2_azimuth = np.radians( self.src_data[ 'sensor_azimuth_angle' ] )
+        
+        x1,y1 = model_space.get_xy( lat, lon )
+        p1 = (x1,y1,0)
+        p0 = model_space.get_ray_top( p1, p0_zenith, p0_azimuth )
+        p2 = model_space.get_ray_top( p1, p2_zenith, p2_azimuth )
+        
+        ray_in = Ray( p0, p1 )
+        ray_out = Ray( p1, p2 )
+        try:
+            in_dict = model_space.grid.get_ray_cell_dist( ray_in )
+            out_dict = model_space.grid.get_ray_cell_dist( ray_out )
+        except AssertionError:
+            self.coord_fail( 'outside grid area' )
+            return None
+        
+        dist_dict = in_dict.copy()
+        for coord, val in out_dict.items():
+            dist_dict[ coord ] = dist_dict.get( coord, 0 ) + val
+        tdist = sum( dist_dict.values() )
+            
+        #convert x-y-z into lay-row-col and scale values so they sum to 1
+        result = { (lay,row,col):val/tdist for [(col,row,lay,),val]
+                   in dist_dict.items() if val > 0.0 }
+        return result
+    
+    def map_time( self, model_space ):
+        #convert source time into [ int(YYYYMMDD), int(HHMMSS) ]
+        fulltime = dt.datetime.utcfromtimestamp( self.src_data[ 'time' ] )
+        day = int( fulltime.strftime( '%Y%m%d' ) )
+        time = int( fulltime.strftime( '%H%M%S' ) )
+        self.time = [ day, time ]
+        #use generalized function
+        return ObsMultiRay.map_time( self, model_space )
+        """
---
+++ obs_preprocess/super_obs_util.py
@@ -0,0 +1,114 @@
+"""
+super_obs_util.py
+
+Copyright 2016 University of Melbourne.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
+"""
+
+from __future__ import absolute_import
+
+import numpy as np
+
+#-CONFIG-SETTINGS-
+
+max_quality_only = True
+surface_type = 1         # 1=land,  0=ocean, -1=both
+operation_mode = -1       # 1=glint, 0=nadir, -1=both
+group_by_second = True
+group_by_column = True
+
+error_ocean = 0.5
+error_land = 0.8
+
+#-----------------
+
+def merge_second( sounding_list ):
+
+    value = np.array( [ s['xco2'] for s in sounding_list ] )
+    unc = np.array( [ s['xco2_uncertainty'] for s in sounding_list ] )
+    
+    def weight_avg( s_list, var ):
+        var_arr = np.array( [ s[var] for s in s_list ] )
+        weights = 1/(unc**2)
+        if var_arr.ndim == 1:
+            w_arr = var_arr * weights
+            sum_w_var = np.sum( w_arr )
+        else:
+            w_arr = np.transpose(var_arr) * weights
+            sum_w_var = np.sum( w_arr, axis=1 )
+        return sum_w_var / np.sum(weights)
+
+    #average of the xco2 uncertainty, assume errors are completely correlated
+    snum = len( sounding_list )
+    avg_var_unc = (np.sum(unc)**2)/snum
+    avg_unc = unc.mean()
+    xco2_w_avg = weight_avg( sounding_list, 'xco2' )
+    spread_ret = np.sqrt( ((xco2_w_avg - value)**2).mean() )
+    
+    #use spread if it is higher than reported uncertainty
+    surface_type = int( sounding_list[0]['surface_type'] )
+    my_err = error_land if surface_type == 1 else error_ocean
+    if spread_ret == 0.:
+        avg_spread = my_err
+    else:
+        avg_spread = max( spread_ret, my_err/float(snum) )
+    avg_unc = max( avg_unc, avg_spread )
+
+    key_list = sounding_list[0].keys()
+    out_dict = {}
+    for k in key_list:
+        if k == 'xco2_uncertainty':
+            out_dict[k] = avg_unc
+        elif k == 'surface_type':
+            out_dict[k] = surface_type
+        elif k == 'operation_mode':
+            out_dict[k] = sounding_list[0]['operation_mode']
+        else:
+            out_dict[k] = weight_avg( sounding_list, k )
+
+    return out_dict
+
+def is_single_column( obs ):
+    col_set = set([ (c[3],c[4],) for c in obs['weight_grid'].keys() ])
+    return (len(col_set) == 1)
+
+def get_col_id( obs ):
+    surface = [ (v,c) for c,v in obs['weight_grid'].items() if c[2]==0 ]
+    coord = max(surface)[1]
+    coord_id = (coord[0], coord[1], coord[3], coord[4],)
+    return coord_id
+
+def merge_column( obs_list ):
+    unc = [ o['uncertainty'] for o in obs_list ]
+    value = [ o['value'] for o in obs_list ]
+    offset = [ o['offset_term'] for o in obs_list ]
+    weight = 1./(np.array(unc)**2)
+    avg_unc = 1./np.sqrt(weight.sum())
+    avg_value = (weight * np.array(value)).sum() / weight.sum()
+    avg_offset = (weight * np.array(offset)).sum() / weight.sum()
+
+    all_coord = set()
+    for o in obs_list:
+        all_coord = all_coord.union( set(o['weight_grid'].keys()) )
+
+    avg_weight_grid = {}
+    for coord in all_coord:
+        c_unc = []
+        c_val = []
+        for o in obs_list:
+            try:
+                c_val.append( o['weight_grid'][coord] )
+                c_unc.append( o['uncertainty'] )
+            except KeyError:
+                pass
+        w_val = ( ( (1./np.array(c_unc)**2) * np.array(c_val) ).sum()
+                  / (1./np.array(c_unc)**2).sum() )
+        avg_weight_grid[coord] = w_val
+    out_dict = { 'uncertainty': avg_unc,
+                 'value': avg_value,
+                 'offset_term': avg_offset,
+                 'weight_grid': avg_weight_grid }
+    return out_dict
