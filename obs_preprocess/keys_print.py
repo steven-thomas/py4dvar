68599d7eb4ab32518c021f625163199ade116a8b
Caitlin Minney <cm5310@gadi-login-02.gadi.nci.org.au>
2021-10-28 09:28:55 +1100

    On branch cminneyCO  Changes to be committed: 	new file:   keys_print.py  Changes not staged for commit: 	modified:   keys_print.py 	modified:   oco2lite_preprocess.py 	modified:   super_obs_util.py 	modified:   ../yohanna_py4dvar/py4dvar (modified content)

---
+++ obs_preprocess/keys_print.py
@@ -0,0 +1,11 @@
+from netCDF4 import Dataset
+import numpy
+from datetime import datetime
+nc = Dataset('/scratch/w22/cm5310/store_share_p4d/obs_TROPOMI_data/s5p_l2_co_0007_03699.nc')
+
+
+t = nc.groups['instrument'].variables['time']
+t_list = t[0].tolist()
+date = datetime(year = t_list[0], month = t_list[1], day = t_list[2], hour = t_list[3], minute = t_list[4], second = t_list[5])
+print(date)
+
